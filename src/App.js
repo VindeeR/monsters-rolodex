74ae1d7dd405016f000ba9b673a88134059de7bd
VindeeR <ericklopes.47@gmail.com>
2020-06-16 05:58:57 -0300

    updating project and fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+
 import { CardList } from './components/card-list/card-list.component';
 import { SearchBox } from './components/search-box/search-box.component';
+
+import './App.css';
 
 class App extends Component{
   constructor(){
@@ -26,7 +29,10 @@ class App extends Component{
     
     return(
       <div className = 'App'>
-        <SearchBox placeholder='search monsters' handleChange={e => this.setState({ searchField: e.target.value })}></SearchBox>
+        <h1>Monster Rolodex</h1>
+        <SearchBox placeholder='search monsters' handleChange={
+          e => this.setState({ searchField: e.target.value }
+        )}></SearchBox>
         <CardList monsters = {filteredMonsters}></CardList>
       </div>
     );
