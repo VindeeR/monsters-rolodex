01a5285f652f2d2de3c7055774d28ba9a9f1dacb
VindeeR <ericklopes.47@gmail.com>
2020-06-16 06:56:36 -0300

    Updates

---
+++ precache-manifest.6b268e577809f0475dee8ce5ba780ce6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2d4f51f6b2df86754e0cd5517039f5de",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "1704e974c7f7a6b2a870",
+    "url": "/monsters-rolodex/static/css/main.5c179ee4.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1704e974c7f7a6b2a870",
+    "url": "/monsters-rolodex/static/js/main.8c947bf2.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
